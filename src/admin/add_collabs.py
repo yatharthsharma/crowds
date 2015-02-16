e7b78a0050ef49906141ad4c00af9df8c2eb71f0
Sharad Goel <5harad.6oel@gmail.com>
2015-02-16 10:21:39 -0800

    tweaked add_collabs.py to deal with pagination; updated github usernames

--- src/admin/add_collabs.py
+++ src/admin/add_collabs.py
@@ -17,17 +17,29 @@ repo = '5harad/crowds'
 with open('.github-creds') as f:
     user, passwd = f.readline().strip().split()
 
-# load collaborators
+# load collaborators to add
 collabs = set([line.strip() for line in sys.stdin.readlines()])
 
 # get the current list of collaborators
+cur_collabs = []
 url = 'https://api.github.com/repos/%s/collaborators' % repo
-r = requests.get(url, auth=(user, passwd))
-cur_collabs = set([item['login'] for item in r.json()])
-
+while url:
+    r = requests.get(url, auth=(user, passwd))
+    cur_collabs += [item['login'] for item in r.json()]
+    
+    url = False
+    if 'link' in r.headers:
+        links = [link.split(';') for link in r.headers['link'].split(',')]
+        links = [(l[0].strip('<>'), l[1].strip()) for l in links]
+        urls = [l[0] for l in links if l[1]=='rel="next"']
+        if len(urls) > 0:
+            url = urls[0]
+            
 # add the additional collaborators
+cur_collabs = set(cur_collabs)
 collabs_to_add = collabs - cur_collabs
 for c in collabs_to_add:
     print 'adding: %s' % c
     url = 'https://api.github.com/repos/%s/collaborators/%s' % (repo, c)
     requests.put(url, auth=(user, passwd))
+
